1b012806de006e6e411d78bc3532ca23a6818c8d
Piotr Stus <pm2024love@gmail.com>
2024-10-16 12:58:25 +0200

    Project structure added.

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+	<v-app id="inspire">
+	  <v-app-bar>
+		<v-app-bar-title>Todo Application</v-app-bar-title>
+	  </v-app-bar>
+  
+	  <v-main>
+		<div class="pa-4">
+			<NuxtPage />
+		</div>	
+	  </v-main>
+	</v-app>
+</template>
+  
+<script setup>
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,33 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+
+export default defineNuxtConfig({
+ devtools: { enabled: false },
+ ssr: false,
+
+ build: {
+ transpile: ['vuetify'],
+},
+imports: {
+  dirs: ['stores'],
+},
+ modules: [
+   (_options, nuxt) => {
+     nuxt.hooks.hook('vite:extendConfig', (config) => {
+       // @ts-expect-error
+       config.plugins.push(vuetify({ autoImport: true }))
+     })
+   },
+   '@pinia/nuxt',
+   'dayjs-nuxt'
+ ],
+ vite: {
+   vue: {
+     template: {
+       transformAssetUrls,
+     },
+   },
+ },
+
+ compatibilityDate: '2024-10-16',
+})
